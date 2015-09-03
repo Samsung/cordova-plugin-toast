34b5b2459aaa49356376c116b9e30ebf8d593a49
Dayoung Kim <da_y.kim@samsung.com>
2015-09-03 19:24:44 +0900

    fix jshint:src

--- src/sectv-tizen/inputdeviceProxy.js
+++ src/sectv-tizen/inputdeviceProxy.js
@@ -1,15 +1,40 @@
+'use strict';
+
 module.exports = {
 	getSupportedKeys: function (success, fail, args) {
-		
+		args = '';
+		var supportedKeys = [];
+		fail = fail || function () {};
+		supportedKeys = tizen.tvinputdevice.getSupportedKeys();
+
+//		settimeout(function(){
+		success(supportedKeys);
+//		}, 0);
+/*		if (!document.getElementById('tvwindowshow')) {
+			element = document.createElement('div');
+			element.id = 'tvwindowshow';
+		} else {
+			element = document.getElementById('tvwindowshow');
+		}*/
 	},
 	getKey: function(success, fail, args){
-
+		//var inputDeviceKey = getKey(args[0]);
+		success = success || function () {};
+		fail = fail || function () {};
+		args = '';
+		//success(inputDeviceKey);
 	},
 	registerKey: function(success, fail, args){
-
+		//tizen.tvinputdevice.registerKey(args[0]);
+		success = success || function () {};
+		fail = fail || function () {};
+		args = '';		
 	},
 	unregisterKey: function(success, fail, args){
-
+		//tizen.tvinputdevice.unregisterKey(args[0]);
+		success = success || function () {};
+		fail = fail || function () {};
+		args = '';	
 	}
 };
 
