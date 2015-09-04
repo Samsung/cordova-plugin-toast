7266d2f34ee1ce533896fd4b294339fb63b2adaf
Dayoung Kim <da_y.kim@samsung.com>
2015-09-04 17:40:57 +0900

    realize all inputdevice function(browser/tizen/orsay)

--- src/sectv-tizen/inputdeviceProxy.js
+++ src/sectv-tizen/inputdeviceProxy.js
@@ -2,39 +2,37 @@
 
 module.exports = {
 	getSupportedKeys: function (success, fail, args) {
-		args = '';
-		var supportedKeys = [];
+		args = args || '';
 		fail = fail || function () {};
+		
+		var supportedKeys = [];
 		supportedKeys = tizen.tvinputdevice.getSupportedKeys();
 
-//		settimeout(function(){
-		success(supportedKeys);
-//		}, 0);
-/*		if (!document.getElementById('tvwindowshow')) {
-			element = document.createElement('div');
-			element.id = 'tvwindowshow';
-		} else {
-			element = document.getElementById('tvwindowshow');
-		}*/
+		setTimeout(function(){
+			success(supportedKeys);
+		}, 0);
 	},
 	getKey: function(success, fail, args){
-		//var inputDeviceKey = getKey(args[0]);
-		success = success || function () {};
 		fail = fail || function () {};
-		args = '';
-		//success(inputDeviceKey);
+		args = args || '';
+
+		var inputDeviceKey = tizen.tvinputdevice.getKey(args[0]);
+
+		success(inputDeviceKey);
 	},
 	registerKey: function(success, fail, args){
-		//tizen.tvinputdevice.registerKey(args[0]);
 		success = success || function () {};
 		fail = fail || function () {};
-		args = '';		
+		args = args || '';
+
+		tizen.tvinputdevice.registerKey(args[0]);
 	},
 	unregisterKey: function(success, fail, args){
-		//tizen.tvinputdevice.unregisterKey(args[0]);
 		success = success || function () {};
 		fail = fail || function () {};
-		args = '';	
+		args = args || '';
+		
+		tizen.tvinputdevice.unregisterKey(args[0]);
 	}
 };
 
