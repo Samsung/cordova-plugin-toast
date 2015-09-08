80d76c97297a019d2b33d2fe943b99e93db0acb2
Sukyung Lee <sk08.lee@samsung.com>
2015-09-08 21:37:37 +0900

    add try/catch statement and develop orsay

--- src/sectv-orsay/tvwindowProxy.js
+++ src/sectv-orsay/tvwindowProxy.js
@@ -2,34 +2,49 @@
 
 module.exports = {
 	getAvailableWindows: function (success, fail, args) {
-		success = null;
-		fail = null;
-		args = null;
+		webapis.tv.window.getAvailableWindow(success, fail);
 	},
 	setSource: function (success, fail, args){
-		success = null;
-		fail = null;
-		args = null;
+		webapis.tv.window.setSource(args[0], success, fail, args[1]);
 	},
 	getSource: function (success, fail, args) {
-		success = null;
-		fail = null;
-		args = null;
+		var source = tizen.tvwindow.getSource(args[0]);
+
+		setTimeout(function () {
+			success(source);
+		}, 0);
 	},
 	show: function (success, fail, args) {
-		success = null;
-		fail = null;
-		args = null;
+		if (webapis.tv.window.setRect(args[0], args[1])) {
+			if (webapis.tv.window.show(args[1])) {
+				setTimeout(function () {
+					success();
+				}, 0);
+			} else {
+				setTimeout(function () {
+					fail();
+				}, 0);
+			}
+		} else {
+			setTimeout(function () {
+				fail();
+			}, 0);
+		}
+		
 	},
 	hide: function (success, fail, args) {
-		success = null;
-		fail = null;
-		args = null;
+		if (webapis.tv.window.hide(args[0])) {
+			setTimeout(function () {
+				success();
+			}, 0);
+		} else {
+			setTimeout(function () {
+				fail();
+			}, 0);
+		}
 	},
 	getRect: function (success, fail, args) {
-		success = null;
-		fail = null;
-		args = null;
+
 	}
 };
 
