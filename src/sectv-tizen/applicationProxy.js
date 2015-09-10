5ee1753fb4afe6e9fd2b0faa0212007188529487
Kookheon Kim <kookheon.kim@samsung.com>
2015-09-10 19:07:18 +0900

    delete sectv-tizen code for yobi buffix(WHYgit rm -r src/sectv-tizengit rm -r src/sectv-tizen????)

--- src/sectv-tizen/applicationProxy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-	exit: function (success, fail, args) {
-		tizen.application.getCurrentApplication().exit();
-	}
-};
-
-require('cordova/exec/proxy').add('toast.application', module.exports);
