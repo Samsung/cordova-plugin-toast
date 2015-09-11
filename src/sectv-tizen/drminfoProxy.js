b00f2d1335992e2beda7be6e7d316adc15cef2be
Hyojin Kim <hyojins.kim@samsung.com>
2015-09-11 17:34:15 +0900

    add drminfo : tizen, browser(not yet), www

--- src/sectv-tizen/drminfoProxy.js
+++ src/sectv-tizen/drminfoProxy.js
@@ -2,19 +2,70 @@
 
 module.exports = {
 	getVersion: function (success, fail, args) {
-		success = null;
-		fail = null;
-		args = null;
+		try {
+			success = success || function () {};
+			fail = fail || function() {};
+			args = args || '';
+
+			var result = webapis.drminfo.getVersion();
+
+			if (typeof result == 'boolean') {
+				setTimeout(function () {
+					success(result);
+				}, 0);
+			} else {
+				var error = new Error();
+				error.name = 'UnknownError';
+				error.message = 'UnknownError';
+				fail(error);
+			}
+		} catch (e) {
+			throw e;
+		}
 	},
 	getEsn: function(success, fail, args){
-		success = null;
-		fail = null;
-		args = null;
+		try {
+			success = success || function () {};
+			fail = fail || function() {};
+			args = args || '';
+
+			var result = webapis.drminfo.getEsn(args[0]);
+
+			if (typeof result == 'boolean') {
+				setTimeout(function () {
+					success(result);
+				}, 0);
+			} else {
+				var error = new Error();
+				error.name = 'UnknownError';
+				error.message = 'UnknownError';
+				fail(error);
+			}
+		} catch (e) {
+			throw e;
+		}
 	},
 	getSdiId: function(success, fail, args){
-		success = null;
-		fail = null;
-		args = null;
+		try {
+			success = success || function () {};
+			fail = fail || function() {};
+			args = args || '';
+
+			var result = webapis.drminfo.getSdiId();
+
+			if (typeof result == 'boolean') {
+				setTimeout(function () {
+					success(result);
+				}, 0);
+			} else {
+				var error = new Error();
+				error.name = 'UnknownError';
+				error.message = 'UnknownError';
+				fail(error);
+			}
+		} catch (e) {
+			throw e;
+		}
 	}
 };
 
