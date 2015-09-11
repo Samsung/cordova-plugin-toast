74d0b3a07665b398b4937a15ea2a9af75696ff0e
Woosik Park <woosik.park@samsung.com>
2015-09-11 19:49:51 +0900

    add mediaProxy : tizen

--- src/sectv-tizen/mediaProxy.js
+++ src/sectv-tizen/mediaProxy.js
@@ -2,73 +2,120 @@
 
 var Media = require('cordova-plugin-toast.media');
 
-var currentVideoinfo = null;
-
 var containerElem = null;
-
-var callbackFnTimer = null;
 
 function createVideContainer(id){
     function setContainerStyleEventListener(elem,callback) {
-        var options = {
-            subtree: false,
-            attributes: true
-        };
-
-        var observer = new MutationObserver(function(mutations) {
+        var containerObserver = new MutationObserver(function(mutations) {
             mutations.forEach(function(e) {
                 callback.call(e.target, e.attributeName);
             });
         });
-        observer.observe(elem, options);
-        Media.onStatus(id,Media.MEDIA_CONTAINER,elem);
+
+        containerObserver.observe(elem, {
+            childList : false,
+            subtree: false,
+            attributes: true
+        });
+        Media.mediaEvent(id,getMediaEventVaule(Media._MEDIA_CONTAINER,elem));
+    }
+
+    function setContainerAppendEventListener(callback) {
+        var bodyObserver = new MutationObserver(function(mutations) {
+            mutations.forEach(function(e) {
+                callback.call(e.target, e);
+            });
+        });
+        bodyObserver.observe(document.body, {
+            childList : true,
+            subtree : true,
+            attributes: false
+        });
     }
 
     if(window.MutationObserver) {
-        var body = document.getElementsByTagName('body')[0];
-        containerElem = document.createElement('OBJECT');
-        containerElem.type = 'application/avplayer';
+        containerElem = document.createElement('div');
         containerElem.style.left = '0px';
         containerElem.style.top =  '0px';
         containerElem.style.width =  '0px';
         containerElem.style.height =  '0px';
-        body.appendChild(containerElem);
+        containerElem.innerHTML = '<OBJECT type="application/avplayer"></OBJECT>';
         setContainerStyleEventListener (containerElem,containerStyleEventCallback);
+        setContainerAppendEventListener (containerAppendEventCallback);
     } else {
         throw Error('The platform does not support toast.media');
     }
 }
 
-function containerStyleEventCallback(target,attributeName) {
-    if(callbackFnTimer){
-        clearTimeout(callbackFnTimer);
+var containerStylecallbackFnTimer = null;
+function containerStyleEventCallback(MutationRecordProperty) {
+    if(containerStylecallbackFnTimer){
+        clearTimeout(containerStylecallbackFnTimer);
     }
-    callbackFnTimer = setTimeout(function(){
-        if (target == 'style'){
-            var boundingRect = getBoundingRect(containerElem);
-            console.log('boundingRect.left.............'+boundingRect.left);
-            console.log('boundingRect.top.............'+boundingRect.top);
-            console.log('boundingRect.width.............'+boundingRect.width);
-            console.log('boundingRect.height.............'+boundingRect.height);
-        
-            currentVideoinfo.displayRect = {
-                'left' : Number(boundingRect.left),
-                'top' : Number(boundingRect.top),
-                'width' : Number(boundingRect.width),
-                'height' : Number(boundingRect.height)
-            };
-
-            try{
-                if(webapis.avplay.getState() == 'IDLE' || webapis.avplay.getState() == 'PAUSED' || webapis.avplay.getState() == 'PLAYING'){
-                    webapis.avplay.setDisplayRect(currentVideoinfo.displayRect.left,currentVideoinfo.displayRect.top,currentVideoinfo.displayRect.width,currentVideoinfo.displayRect.height);
-                }
-            } catch (e){
-                console.log('[Warning]Fail to setDisplayRect' + e);
-            }
-            
+    containerStylecallbackFnTimer = setTimeout(function(){
+        if (MutationRecordProperty == 'style'){
+            containerElem.childNodes[0].style.width = containerElem.style.width;
+            containerElem.childNodes[0].style.height = containerElem.style.height;
+            setAvplayVideoRect(containerElem);
         }
     },0);
 }
+
+var containerAppendcallbackFnTimer = null;
+function containerAppendEventCallback(MutationRecordProperty){
+    if(containerAppendcallbackFnTimer){
+        clearTimeout(containerAppendcallbackFnTimer);
+    }
+    
+    containerAppendcallbackFnTimer = setTimeout(function(){
+        if (MutationRecordProperty.addedNodes.length > 0) {
+            console.log('addedNodes.............');
+            if(hasContainerElem(MutationRecordProperty.addedNodes)){
+                console.log('append containerElem.............');
+                setAvplayVideoRect(containerElem);
+            }
+        }
+    },0);
+
+    function hasContainerElem(nodes){
+        for(var i = 0 ; i < nodes.length ; i++){
+            if(containerElem === nodes[i] || isChildOf(containerElem,nodes[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+function isChildOf(element, parent) {
+    if (!(element instanceof HTMLElement) || !element.parentElement) {
+        return false;
+    }
+    if (element.parentElement === parent) {
+        return true;
+    }
+    else {
+        return isChildOf(element.parentElement, parent);
+    }
+}
+
+function setAvplayVideoRect(element){
+    var boundingRect = getBoundingRect(element);
+    console.log('boundingRect.left.............'+boundingRect.left);
+    console.log('boundingRect.top.............'+boundingRect.top);
+    console.log('boundingRect.width.............'+boundingRect.width);
+    console.log('boundingRect.height.............'+boundingRect.height);
+
+    try{
+        var state = webapis.avplay.getState();
+        if(state == 'IDLE' || state == 'PAUSED' || state == 'PLAYING' || state =='READY'){
+            webapis.avplay.setDisplayRect(Number(boundingRect.left),Number(boundingRect.top),Number(boundingRect.width),Number(boundingRect.height));
+        }
+    } catch (e){
+        console.log('[Warning]Fail to setDisplayRect' + e);
+    }
+}
+
 
 function getBoundingRect (el) {
     var width = 0, height = 0, left = 0, top = 0;
@@ -133,128 +180,178 @@ function camelCase(input) {
     return camelCache[input];
 }
 
-
-function createAvplay(id,src) {
-    setTimeout(function(){
-        if(window.webapis){
-            currentVideoinfo.id = id;
-            currentVideoinfo.src = src;
-            webapis.avplay.open(src);
-            webapis.avplay.setListener({
-                onbufferingstart: function() {
-                    console.log('Buffering start.');
-                    Media.onStatus(id,Media.MEDIA_STATE,Media.MEDIA_BUFFERINGSTART);
-                },
-                onbufferingprogress: function(percent) {
-                    console.log('Buffering progress data : ' + percent);
-                    Media.onStatus(id,Media.MEDIA_BUFFERINGPROGRESS,percent);
-                },
-                onbufferingcomplete: function() {
-                    console.log('Buffering complete.');
-                    Media.onStatus(id,Media.MEDIA_STATE,Media.MEDIA_BUFFERINGCOMPLETE);
-                },
-                onstreamcompleted: function(currentTime) {
-                    webapis.avplay.stop();
-                    Media.onStatus(id,Media.MEDIA_STATE,Media.MEDIA_STOPPED);
-                },
-                oncurrentplaytime: function(currentTime) {
-                    console.log('Current playtime: ' + currentTime);
-                    Media.onStatus(id,Media.MEDIA_POSITION,currentTime);
-                },
-                onevent: function(eventType, eventData) {
-                    console.log('Event type error : ' + eventType + ', eventData: ' + eventData);
-                },
-                onerror: function(errorData) {
-                    console.log('Event type error : ' + errorData);
-                    Media.onStatus(id,Media.MEDIA_ERROR,errorData);
-                },
-                onsubtitlechange: function(duration, text, data1, data2) {
-                    console.log('Subtitle Changed.');
-                    Media.onStatus(id,Media.MEDIA_SUBTITLE,[text, data1, data2]);
-                },
-                ondrmevent: function(drmEvent, drmData) {
-                    console.log('DRM callback: ' + drmEvent + ', data: ' + drmData);
-                }
-            });
-        }
-    },0);
-}
-
+var currentVideoinfo = null;
 function setInitVideoInfo(){
     currentVideoinfo = null;
     currentVideoinfo = {
         id : null,
-        src : null,
-        displayRect : {
-            left : '0',
-            top : '0',
-            width : window.document.documentElement.clientWidth,
-            height : window.document.documentElement.clientHeight
-        }
+        src : null
     };
+}
+var currentVideoState = null;
+function getMediaEventVaule (type,data) {
+    var reval = {};
+    switch(type){
+    case Media.EVENT_STATE :
+        reval = {
+            'type' : type,
+            'data' : {
+                'state' : data,
+                'oldState' : currentVideoState
+            }
+        };
+        currentVideoState = data;
+        break;
+    case Media.EVENT_DURATION :
+        reval = {
+            'type' : type,
+            'data' : {
+                'duration' : data
+            }
+        };
+        break;
+    case Media.EVENT_POSITION :
+        reval = {
+            'type' : type,
+            'data' : {
+                'position' : data
+            }
+        };
+        break;
+    case Media.EVENT_BUFFERINGPROGRESS :
+         reval = {
+            'type' : type,
+            'data' : {
+                'bufferingPercentage' : data
+            }
+        };
+        break;
+    case Media._MEDIA_CONTAINER :
+        reval = {
+            'type' : type,
+            'data' : {
+                'containerElem' : data
+            }
+        };
+        break;        
+    case Media._MEDIA_ERROR :
+        reval = {
+            'type' : type,
+            'data' : data
+        };
+        break;    
+    }
+    return reval;
 }
 
 module.exports = {
     create:function(successCallback, errorCallback, args) {
-        var id = args[0], src = args[1], containerId = args[2];
-        console.log('media::create() - id =' + id + ', src =' + src + ', containerId=' + containerId);
+        var id = args[0];
+        console.log('media::create() - id =' + id);
         setInitVideoInfo();
-        createAvplay(id,src);
-        createVideContainer(id,containerId);
+        createVideContainer(id);
     },
 
-    // Start playing the video
-    startPlayingVideo:function(successCallback, errorCallback, args) {
-        var id = args[0], src = args[1], options = args[2];
+    open:function(successCallback, errorCallback, args) {
+        var id = args[0], src = args[1];
 
-        console.log('media::startPlayingVideo() - id =' + id + ', src =' + src + ', options =' + options);
-
-        if(currentVideoinfo.src != src){
-            createAvplay(id,src);
-        }
-
+        console.log('media::open() - id =' + id);
         setTimeout(function(){
-            if(webapis.avplay.getState() == 'PAUSED') {
-                webapis.avplay.play();
-            } else if(webapis.avplay.getState() == 'IDLE'){
-                containerElem.style.display = 'block';
-                containerElem.style.left = currentVideoinfo.displayRect.left + 'px';
-                containerElem.style.top = currentVideoinfo.displayRect.top + 'px';
-                containerElem.style.width = currentVideoinfo.displayRect.width + 'px';
-                containerElem.style.height = currentVideoinfo.displayRect.height + 'px';
-                try{
-                    webapis.avplay.setDisplayRect(currentVideoinfo.displayRect.left,currentVideoinfo.displayRect.top,currentVideoinfo.displayRect.width,currentVideoinfo.displayRect.height);
-                } catch(e){
-                    errorCallback(e);
-                }
-                    
-                try{
-                    webapis.avplay.prepareAsync(function(){
-                        webapis.avplay.play();
-                        Media.onStatus(id,Media.MEDIA_DURATION,webapis.avplay.getDuration());
-                    },function(e){
-                        errorCallback(e);
-                    });
-                } catch(e) {
-                    errorCallback(e);
-                }
+            if(window.webapis){
+                currentVideoinfo.id = id;
+                currentVideoinfo.src = src;
+                webapis.avplay.open(src);
+                webapis.avplay.setListener({
+                    onbufferingstart: function() {
+                        console.log('media::onStalled()');
+                        Media.mediaEvent(id,getMediaEventVaule(Media.EVENT_STATE,Media.STATE_STALLED));
+                    },
+                    onbufferingprogress: function(percent) {
+                        console.log('Buffering progress data : ' + percent);
+                        Media.mediaEvent(id,getMediaEventVaule(Media.EVENT_BUFFERINGPROGRESS,percent));
+                    },
+                    onbufferingcomplete: function() {
+                        console.log('Buffering complete.');
+                        var state = webapis.avplay.getState();
+                        if(state == 'READY') {
+                            Media.mediaEvent(id,getMediaEventVaule(Media.EVENT_STATE,Media.STATE_IDLE));
+                        } else {
+                            Media.mediaEvent(id,getMediaEventVaule(Media.EVENT_STATE,state));
+                        }
+                        Media.mediaEvent(id,getMediaEventVaule(Media.EVENT_POSITION,webapis.avplay.getCurrentTime()));
+                    },
+                    onstreamcompleted: function(currentTime) {
+                        console.log('edia::streamcompleted()');
+                        webapis.avplay.stop();
+                        Media.mediaEvent(id, getMediaEventVaule(Media.EVENT_STATE, Media.STATE_IDLE));
+                    },
+                    oncurrentplaytime: function(currentTime) {
+                        console.log('Current playtime: ' + currentTime);
+                        Media.mediaEvent(id,getMediaEventVaule(Media.EVENT_POSITION,currentTime));
+                    },
+                    onevent: function(eventType, eventData) {
+                        console.log('Event type error : ' + eventType + ', eventData: ' + eventData);
+                    },
+                    onerror: function(errorData) {
+                        console.log('Event type error : ' + errorData);
+                        Media.mediaEvent(id,getMediaEventVaule(Media._MEDIA_ERROR,errorData));
+                    },
+                    onsubtitlechange: function(duration, text, data1, data2) {
+                        console.log('Subtitle Changed.');
+                    },
+                    ondrmevent: function(drmEvent, drmData) {
+                        console.log('DRM callback: ' + drmEvent + ', data: ' + drmData);
+                    }
+                });
+                currentVideoState = Media.STATE_IDLE;
+                Media.mediaEvent(id, getMediaEventVaule(Media.EVENT_STATE, Media.STATE_IDLE));
             }
         },0);
     },
 
-    // Stops the playing video
-    stopPlayingVideo:function(successCallback, errorCallback, args) {
+    // Start playing the video
+    play:function(successCallback, errorCallback, args) {
         var id = args[0];
-        webapis.avplay.stop();
-        Media.onStatus(id, Media.MEDIA_STATE, Media.MEDIA_STOPPED);
+
+        console.log('media::play() - id =' + id);
+
+        setTimeout(function(){
+            if(webapis.avplay.getState() == 'IDLE'){
+                webapis.avplay.prepareAsync(function(){
+                    webapis.avplay.play();
+                    Media.mediaEvent(id, getMediaEventVaule(Media.EVENT_STATE, Media.STATE_PLAYING));
+                    Media.mediaEvent(id,getMediaEventVaule(Media.EVENT_DURATION,webapis.avplay.getDuration()));
+                },function(e){
+                    throw Error ('Failed to open media');
+                });
+            } else {
+                webapis.avplay.play();
+                Media.mediaEvent(id, getMediaEventVaule(Media.EVENT_STATE, Media.STATE_PLAYING));
+            }
+
+        },0);
+    },
+
+    // Stops the playing video
+    stop:function(successCallback, errorCallback, args) {
+        var id = args[0];
+        console.log('media::stop() - EVENT_STATE -> IDLE');
+        try {
+            webapis.avplay.stop();
+            Media.mediaEvent(id, getMediaEventVaule(Media.EVENT_STATE, Media.STATE_IDLE));
+            successCallback();
+        } catch (e){
+            throw e;
+        }
+        
     },
 
     // Seeks to the position in the video
-    seekToVideo:function(successCallback, errorCallback, args) {
+    seekTo:function(successCallback, errorCallback, args) {
         //var id = args[0];
         var milliseconds = args[1];
 
-        console.log('media::seekToVideo()');
+        console.log('media::seekTo()');
         try {
             webapis.avplay.seekTo(milliseconds,function(time){
                 setTimeout(function(){
@@ -262,29 +359,23 @@ module.exports = {
                 },0);
                 
             },function(){
-                errorCallback();
+                throw Error ('Failed to seekTo');
             });
         } catch(e) {
-            errorCallback(e);
+            throw e;
         }
     },
 
     // Pauses the playing video
-    pausePlayingVideo:function(successCallback, errorCallback, args) {
+    pause:function(successCallback, errorCallback, args) {
         var id = args[0];
-
-        console.log('media::pausePlayingVideo() - MEDIA_STATE -> MEDIA_PAUSED');
-        webapis.avplay.pause();
-        Media.onStatus(id, Media.MEDIA_STATE, Media.MEDIA_PAUSED);
-    },
-
-    // Gets current position in the video
-    getCurrentPositionVideo:function(successCallback, errorCallback, args) {
-        //var id = args[0];
-        console.log('media::getCurrentPositionVideo()');
-        setTimeout(function(){
-            successCallback(webapis.avplay.getCurrentTime());
-        },0);
+        console.log('media::pause() - EVENT_STATE -> PAUSED');
+        try {
+            webapis.avplay.pause();
+            Media.mediaEvent(id, getMediaEventVaule(Media.EVENT_STATE, Media.STATE_PAUSED));
+        } catch(e){
+            throw e;
+        }
     }
 };
 
