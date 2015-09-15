4805d1d0139f06e75b0f1622d40fa087de11f1f1
Hyojin Kim <hyojins.kim@samsung.com>
2015-09-15 17:40:34 +0900

    add testsuite for drminfo, tvaudiocontrol

---
+++ cordova-test-runner/testsuite/toast.drminfo.js
@@ -0,0 +1,24 @@
+/* globals testsuite */
+(function() {
+    testsuite('toast.drminfo', 'getVersion()', function(report) {
+        toast.drminfo.getVersion(function(value) {
+            report('Success: ' + value);
+        }, function(err) {
+            report('Error: ' + JSON.stringify(err));
+        });
+    });
+    testsuite('toast.drminfo', 'getEsn() to \'WIDEVINE\'', function(report) {
+        toast.drminfo.getEsn(function(value) {
+            report('Success: ' + value);
+        }, function(err) {
+            report('Error: ' + JSON.stringify(err));
+        }, 'WIDEVINE');
+    });
+    testsuite('toast.drminfo', 'getSdiId()', function(report) {
+        toast.drminfo.getSdiId(function(value) {
+            report('Success: ' + value);
+        }, function(err) {
+            report('Error: ' + JSON.stringify(err));
+        });
+    });
+})();
---
+++ cordova-test-runner/testsuite/toast.tvaudiocontrol.js
@@ -0,0 +1,46 @@
+/* globals testsuite */
+(function() {
+    testsuite('toast.tvaudiocontrol', 'setMute() to \'true\'', function(report) {
+        toast.tvaudiocontrol.setMute(true);
+        report('OK');
+    });
+    testsuite('toast.tvaudiocontrol', 'setMute() to \'false\'', function(report) {
+        toast.tvaudiocontrol.setMute(false);
+        report('OK');
+    });
+    testsuite('toast.tvaudiocontrol', 'isMute()', function(report) {
+        toast.tvaudiocontrol.isMute(function(value) {
+            report('Success: ' + value);
+        }, function(err) {
+            report('Error: ' + JSON.stringify(err));
+        });
+    });
+    testsuite('toast.tvaudiocontrol', 'setVolume() to \'5\'', function(report) {
+        toast.tvaudiocontrol.setSource(5);
+        report('OK');
+    });
+    testsuite('toast.tvaudiocontrol', 'setVolumeUp()', function(report) {
+        toast.tvaudiocontrol.setVolumeUp();
+        report('OK');
+    });
+    testsuite('toast.tvaudiocontrol', 'setVolumeDown()', function(report) {
+        toast.tvaudiocontrol.setVolumeDown();
+        report('OK');
+    });
+    testsuite('toast.tvaudiocontrol', 'getVolume()', function(report) {
+        toast.tvaudiocontrol.getVolume(function(value) {
+            report('Success: ' + value);
+        }, function(err) {
+            report('Error: ' + JSON.stringify(err));
+        });
+    });
+    testsuite('toast.tvaudiocontrol', 'setVolumeChangeListener()', function(report) {
+        toast.tvaudiocontrol.setVolumeChangeListener(function(value) {
+            report('Success: volume changes to ' + value);
+        });
+    });
+    testsuite('toast.tvaudiocontrol', 'unsetVolumeChangeListener()', function(report) {
+        toast.tvaudiocontrol.unsetVolumeChangeListener();
+        report('OK');
+    });
+})();
