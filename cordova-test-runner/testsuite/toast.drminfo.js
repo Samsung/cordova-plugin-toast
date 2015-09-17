ac9eb741cbb3c7b71d70661822ca231b372701d2
Hyojin Kim <hyojins.kim@samsung.com>
2015-09-17 15:24:33 +0900

    modify testsuite as meeting spec doc

--- cordova-test-runner/testsuite/toast.drminfo.js
+++ cordova-test-runner/testsuite/toast.drminfo.js
@@ -1,12 +1,5 @@
 /* globals testsuite */
 (function() {
-    testsuite('toast.drminfo', 'getVersion()', function(report) {
-        toast.drminfo.getVersion(function(value) {
-            report('Success: ' + value);
-        }, function(err) {
-            report('Error: ' + JSON.stringify(err));
-        });
-    });
     testsuite('toast.drminfo', 'getEsn() to \'WIDEVINE\'', function(report) {
         toast.drminfo.getEsn('WIDEVINE', function(value) {
             report('Success: ' + value);
@@ -14,8 +7,8 @@
             report('Error: ' + JSON.stringify(err));
         });
     });
-    testsuite('toast.drminfo', 'getSdiId()', function(report) {
-        toast.drminfo.getSdiId(function(value) {
+    testsuite('toast.drminfo', 'getSdi()', function(report) {
+        toast.drminfo.getSdi(function(value) {
             report('Success: ' + value);
         }, function(err) {
             report('Error: ' + JSON.stringify(err));
