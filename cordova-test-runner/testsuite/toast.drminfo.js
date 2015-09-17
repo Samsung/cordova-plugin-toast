85dbdd964001adf1977e5f1fbeef3bfe76753f2a
Hyojin Kim <hyojins.kim@samsung.com>
2015-09-17 10:29:24 +0900

    bugfix jscs : testsuite/toast.drminfo.js, testsuite/toast.tvaudiocontrol.js

--- cordova-test-runner/testsuite/toast.drminfo.js
+++ cordova-test-runner/testsuite/toast.drminfo.js
@@ -8,7 +8,7 @@
         });
     });
     testsuite('toast.drminfo', 'getEsn() to \'WIDEVINE\'', function(report) {
-        toast.drminfo.getEsn('WIDEVINE' , function(value) {
+        toast.drminfo.getEsn('WIDEVINE', function(value) {
             report('Success: ' + value);
         }, function(err) {
             report('Error: ' + JSON.stringify(err));
