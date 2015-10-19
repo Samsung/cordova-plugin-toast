9df7dd150a62df558d19289815ad33eb0e675770
Hyojin Kim <hyojins.kim@samsung.com>
2015-10-19 10:10:03 +0900

    update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,5 +68,5 @@ module.exports = function(grunt) {
   grunt.loadTasks('tasks');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint:src', 'jscs:src', 'compile', 'jshint:build']);
+  grunt.registerTask('default', ['jshint:src', 'compile', 'jshint:build', 'jscs']);
 };
