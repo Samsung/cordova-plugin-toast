7f82c8c63ea5e3bb7370f9509bcd13a002c5585b
Hyojin Kim <hyojins.kim@samsung.com>
2015-12-02 18:41:16 +0900

    Add deviceinfo tutorial

---
+++ sample/deviceinfo/js/deviceinfo.js
@@ -0,0 +1,75 @@
+// Element
+var noteEl = null;
+var deviceTextEl = null;
+var networkTextEl = null;
+
+function showDeviceInfoScene() {
+    var sceneEl = document.getElementById('deviceinfo_scene');
+    var appEl = document.getElementById('app');
+    noteEl = document.getElementById('note_text');
+    deviceTextEl = document.getElementById('device_text');
+    networkTextEl = document.getElementById('network_text');
+    
+    appEl.style.visibility = 'hidden';
+    sceneEl.style.visibility = 'visible';
+
+    // add button event : clear_btn
+    document.getElementById('clear_btn').addEventListener('click', function() {
+        toastLog('[Button Event] clicked clear_btn');
+        deviceTextEl.innerHTML = '';
+        networkTextEl.innerHTML = '';
+    });
+
+    // add button event : deviceinfo_btn
+    document.getElementById('deviceinfo_btn').addEventListener('click', function() {
+        toastLog('[Button Event] clicked deviceinfo_btn');
+        displayDeviceInfo();
+    });
+
+    // add button event : networkstatus_btn
+    document.getElementById('networkstatus_btn').addEventListener('click', function() {
+        toastLog('[Button Event] clicked networkstatus_btn');
+        checkConnection();
+    });
+}
+
+function displayDeviceInfo() {
+    deviceTextEl.innerHTML = '';
+
+    for(var key in device) {
+        deviceTextEl.innerHTML += '<p>' + key + ' : ' + device[key] + '</p>';    
+    }
+}
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    toastLog('Connection type: ' + states[networkState]);
+    networkTextEl.innerHTML += '<p> status : ' + states[networkState] + '</p>';
+}
+
+// for debugging
+function toastLog(msg) {
+    var now = new Date();
+    var time = now.toJSON();
+    var debugMsg = '[toast deviceinfo tutorial] (' + time + ') : <br>' + msg;
+    
+    console.log(debugMsg);
+    
+    var debugEl = document.getElementById('debug_container');
+    if(debugEl.scrollHeight > 0) {
+        debugEl.scrollTop = debugEl.scrollHeight/2;
+    }
+
+    debugEl.innerHTML += '<p>' + debugMsg + '</p>';
+}
---
+++ sample/deviceinfo/js/index.js
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        registerKey();
+
+        document.getElementById('ready').addEventListener('click', function(){
+            showDeviceInfoScene();
+        });
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        toastLog('Received Event: ' + id);
+    }
+};
+
+// toast tv keycode
+var tvKeyCode = [];
+
+function registerKey() {
+    toastLog('function registerKey');
+
+    toast.inputdevice.getSupportedKeys(function(keys) {
+        // get supported keys
+        var len = 0;
+        for(var i = 0, len = keys.length; i < len; i++) {
+            tvKeyCode[keys[i].name] = keys[i].code;
+        }
+    });
+
+    window.addEventListener('keydown', function(e) {
+        switch(e.keyCode) {
+            case tvKeyCode.Return:
+                toast.application.exit();
+                break;
+            case tvKeyCode.Enter:
+                showDeviceInfoScene();
+                break;
+        }
+    });
+}
+
+app.initialize();
