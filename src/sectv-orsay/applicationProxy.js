15b36b1775a22c7a1bc09ae4bdf38531f4fcd86c
Dayoung Kim <da_y.kim@samsung.com>
2016-01-28 19:35:32 +0900

    Add launchApp() / getRequestedAppInfo() APIs

--- src/sectv-orsay/applicationProxy.js
+++ src/sectv-orsay/applicationProxy.js
@@ -3,6 +3,66 @@
 module.exports = {
     exit: function (success, fail, args) {
         curWidget.setPreference('return', 'true');
+    },
+    launchApp: function (success, fail, args) {
+        try {
+            var paramAppId = args[0].appId;
+            var paramData = args[0].data;
+            var paramDataKeys = Object.keys(paramData);
+
+            var webbrowserType = '3010'; // Common.API.EVENT_ENUM.RUN_WEBBROWSER
+            var widgetType = '02'; // Common.API.EVENT_ENUM.RUN_SEARCH_WIDGET
+
+            if(paramAppId == '29_fullbrowser') {
+                var data = window.curWidget.id;
+                if(paramData) {
+                    data += '|?|' + paramData[paramDataKeys[0]];
+                }
+                var widgetEvent = new WidgetEvent(webbrowserType, data);
+                sendWidgetEvent('', widgetEvent, false);
+            }
+            else {
+                var data = window.curWidget.id + '?' + encodeURIComponent(paramAppId) + '?' + '&toast_data=' + encodeURIComponent(JSON.stringify(paramData));
+
+                var widgetEvent = new WidgetEvent(widgetType, data);
+                sendWidgetEvent('', widgetEvent, false);
+            }
+            setTimeout(function() {
+                success();
+            }, 0);
+        }
+        catch (e) {
+            var error = new Error(e.message);
+            error.name = e.name;
+            setTimeout(function() {
+                fail(error);
+            }, 0);
+        }
+    },
+    getRequestedAppInfo: function (success, fail, args) {
+        try {
+            var originalData = window.location.search;
+            var reqCallerAppId = parseAppData(originalData, '&callerid=');
+            var reqData = parseAppData(originalData, '&toast_data=');
+
+            var reqDataObj = JSON.parse((decodeURIComponent(reqData)));
+
+            setTimeout(function() {
+                success({callerAppId: decodeURIComponent(reqCallerAppId), data: reqDataObj});
+            }, 0);
+        }
+        catch (e) {
+            var error = new Error(e.message);
+            error.name = e.name;
+            setTimeout(function() {
+                fail(error);
+            }, 0);
+        }
+
+        function parseAppData(originalData, searchData) {
+            var str = originalData.substring(originalData.indexOf(searchData) + 1);
+            return str.substring(str.indexOf('=') + 1, str.indexOf('&'));
+        }
     }
 };
 
--- src/sectv-tizen/applicationProxy.js
+++ src/sectv-tizen/applicationProxy.js
@@ -3,6 +3,53 @@
 module.exports = {
     exit: function (success, fail, args) {
         tizen.application.getCurrentApplication().exit();
+    },
+    launchApp: function (success, fail, args) {
+        try {
+            var paramAppId = args[0].appId;
+            var paramData = args[0].data;
+            var appCtrlDataAry = [];
+
+            for(var keyName in paramData) {
+                var temp = new tizen.ApplicationControlData(keyName, [paramData[keyName]]);
+                appCtrlDataAry.push(temp);
+            }
+
+            var appCtrl = new tizen.ApplicationControl(null, null, null, null, appCtrlDataAry);
+            tizen.application.launchAppControl(appCtrl, paramAppId, success, fail, null);
+        }
+        catch (e) {
+            var error = new Error();
+            error.name = e.name;
+            setTimeout(function() {
+                fail(error);
+            }, 0);
+        }
+    },
+    getRequestedAppInfo: function (success, fail, args) {
+        try {
+            var reqAppCtrl = tizen.application.getCurrentApplication().getRequestedAppControl();
+            var reqCallerAppId = reqAppCtrl.callerAppId;
+            var reqAppCtrlDataAry = reqAppCtrl.appControl.data;
+            var appCtrlDataObj = {};
+
+            for(var i = 0; i < reqAppCtrlDataAry.length; i++) {
+                var key = reqAppCtrlDataAry[i].key;
+                var value = (reqAppCtrlDataAry[i].value)[0];
+                appCtrlDataObj[key] = value;
+            }
+
+            setTimeout(function() {
+                success({callerAppId: reqCallerAppId, data: appCtrlDataObj});
+            }, 0);
+        }
+        catch(e) {
+            var error = new Error();
+            error.name = e.name;
+            setTimeout(function() {
+                fail(error);
+            }, 0);
+        }
     }
 };
 
