e93b35e9191883c3faed635d55e77298db654285
Dayoung Kim <da_y.kim@samsung.com>
2016-02-12 14:07:15 +0900

    Add launchApp()/ getRequestedAppInfo() APIs

--- www/application.js
+++ www/application.js
@@ -1,9 +1,32 @@
 'use strict';
 
-var exec = require('cordova/exec');
+var argscheck = require('cordova/argscheck'),
+    exec = require('cordova/exec');
 
 module.exports = {
     exit: function () {
         exec(null, null, 'toast.application', 'exit', null);
+    },
+    launchApp: function (appInfo, successCallback, errorCallback) {
+        argscheck.checkArgs('ofF', 'application.launchApp', arguments);
+        if(!appInfo.appId || typeof appInfo.appId != 'string') {
+            throw new TypeError('appControl.appId is not a string');
+        }
+
+        appInfo.data = appInfo.data || {};
+        if(typeof appInfo.data != 'object') {
+            throw new TypeError('appControl.data is not a object');
+        }
+
+        var args = [appInfo];
+        exec(successCallback, errorCallback, 'toast.application', 'launchApp', args);
+    },
+    getRequestedAppInfo: function (successCallback, errorCallback) {
+        argscheck.checkArgs('fF', 'application.getRequestedAppInfo', arguments);
+
+        errorCallback = errorCallback || function () {};
+
+        var args = [];
+        exec(successCallback, errorCallback, 'toast.application', 'getRequestedAppInfo', args);
     }
 };
