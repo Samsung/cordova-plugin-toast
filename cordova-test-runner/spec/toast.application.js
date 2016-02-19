8c88c84b6d769f3fd33559c183fb8d40d375615b
Dayoung Kim <da_y.kim@samsung.com>
2016-02-19 18:24:22 +0900

    Add launchApp/getRequestedAppInfo in Jasmine(spec)

--- cordova-test-runner/spec/toast.application.js
+++ cordova-test-runner/spec/toast.application.js
@@ -3,14 +3,25 @@ describe('toast.application', function () {
         expect(window.toast).toBeDefined();
         expect(window.toast.application).toBeDefined();
     });
-
     it('should contain a "exit" function.', function () {
         expect(window.toast.application.exit).toBeDefined();
         expect(typeof window.toast.application.exit).toBe('function');
     });
     it('should not contain a property that is not exists in the specs.', function () {
         for(var prop in toast.application) {
-            expect(['exit'].indexOf(prop) >= 0).toBeTruthy();
+            expect([
+                'exit',
+                'launchApp',
+                'getRequestedAppInfo'
+            ].indexOf(prop) >= 0).toBeTruthy();
         }
+    });
+    it('should contain a "launchApp" function.', function () {
+        expect(window.toast.application.launchApp).toBeDefined();
+        expect(typeof window.toast.application.launchApp).toBe('function');
+    });
+    it('should contain a "getRequestedAppInfo" function.', function () {
+        expect(window.toast.application.getRequestedAppInfo).toBeDefined();
+        expect(typeof window.toast.application.getRequestedAppInfo).toBe('function');
     });
 });
