9ab46a66e9e8f3d4650023d72dfa9e1ea87a5d6d
Dayoung Kim <da_y.kim@samsung.com>
2016-10-24 15:30:29 +0900

    Update the script file for using on MAC

--- script/shell/Getting_Started.sh
+++ script/shell/Getting_Started.sh
@@ -44,8 +44,8 @@ cd ..
 
 echo "================ Add compile task in 'Gruntfile.js'"
 cd cordova-js
-sed -e "s/compile: {/compile: {\n\t\t\t\"sectv-orsay\": {},\n\t\t\t\"sectv-tizen\": {},\n\t\t\t\"tv-webos\": {},/g" Gruntfile.js > Gruntfile.js.tmp && mv Gruntfile.js.tmp Gruntfile.js
-sed -e "s/\"cordova-platforms\" : {/\"cordova-platforms\": {\n\t\t\"cordova-sectv-orsay\"\t\t: \"..\/cordova-sectv-orsay\",\n\t\t\"cordova-sectv-tizen\"\t\t: \"..\/cordova-sectv-tizen\",\n\t\t\"cordova-tv-webos\"\t\t\t: \"..\/cordova-tv-webos\",/g" package.json > package.json.tmp && mv package.json.tmp package.json
+sed -e "s/compile: {/compile: {\"sectv-orsay\": {},\"sectv-tizen\": {},\"tv-webos\": {},/g" Gruntfile.js > Gruntfile.js.tmp && mv Gruntfile.js.tmp Gruntfile.js
+sed -e "s/\"cordova-platforms\" : {/\"cordova-platforms\": {\"cordova-sectv-orsay\": \"..\/cordova-sectv-orsay\",\"cordova-sectv-tizen\": \"..\/cordova-sectv-tizen\",\"cordova-tv-webos\": \"..\/cordova-tv-webos\",/g" package.json > package.json.tmp && mv package.json.tmp package.json
 cd ..
 
 echo "================ Compile project"
