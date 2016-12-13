5d23a4d0459fc5be4b1ab2dd4015d15069e855a6
Dayoung Kim <da_y.kim@samsung.com>
2016-12-13 11:19:49 +0900

    Fix launchApp api for 2012 devices

--- src/sectv-orsay/applicationProxy.js
+++ src/sectv-orsay/applicationProxy.js
@@ -14,16 +14,20 @@ module.exports = {
             var webbrowserType = '3010'; // Common.API.EVENT_ENUM.RUN_WEBBROWSER
 
             /*jshint undef: false */
-            if(device.version < 'T-INFOLINK2013-9999') { // for 2013'
+            if(device.version < 'T-INFOLINK2012-9999') { // for 2012'
+                webbrowserType = '02'; // Common.API.EVENT_ENUM.RUN_SEARCH_WIDGET
+            }
+            else if(device.version < 'T-INFOLINK2013-9999') { // for 2013'
                 webbrowserType = '3008'; // Common.API.EVENT_ENUM.RUN_WEBBROWSER
             }
 
             if(paramAppId == '29_fullbrowser') {
                 var browserData = window.curWidget.id;
-                if(paramData) {
-                    browserData += '|?|' + paramData[paramDataKeys[0]];
+                if(webbrowserType == '02') { // for 2012'
+                    browserData += '|?|' + '29_fullbrowser';
                 }
-
+                browserData += '|?|' + paramData[paramDataKeys[0]];
+                
                 /*jshint undef: false */
                 var browserWidgetEvent = new WidgetEvent(webbrowserType, browserData);
                 sendWidgetEvent('', browserWidgetEvent, false);
